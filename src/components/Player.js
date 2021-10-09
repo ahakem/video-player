6d6d5dec8f84ec6a575bf5d1163a304193ebd3e9
Ahmed Hakim <me@hakim.ninja>
2021-10-09 21:04:14 +0200

    indentation

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,32 +1,18 @@
-import React, {useRef} from "react";
-import {
-  Box,
-  Card,
-  CardMedia,
-  IconButton,
-  Container,
-} from "@mui/material";
+import React, { useRef } from "react";
+import { Box, Card, CardMedia, IconButton, Container } from "@mui/material";
 import usePlayer from "../hooks/usePlayer";
 
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
-import PauseIcon from '@mui/icons-material/Pause';
-import FastForwardIcon from '@mui/icons-material/FastForward';
-import FastRewindIcon from '@mui/icons-material/FastRewind';
-import SpeedIcon from '@mui/icons-material/Speed';
-import LinearProgress from '@mui/material/LinearProgress';
+import PauseIcon from "@mui/icons-material/Pause";
+import FastForwardIcon from "@mui/icons-material/FastForward";
+import FastRewindIcon from "@mui/icons-material/FastRewind";
+import SpeedIcon from "@mui/icons-material/Speed";
+import LinearProgress from "@mui/material/LinearProgress";
 
 export default function Player() {
   const videoElement = useRef(null);
-  const {
-    playerState,
-    togglePlay,
-    handleSpeed,
-    handleLiveProgress
-  } = usePlayer(videoElement);
-
-
-
- 
+  const { playerState, togglePlay, handleSpeed, handleLiveProgress } =
+    usePlayer(videoElement);
   return (
     <Container maxWidth="md">
       <Card sx={{ display: "flex", flexDirection: "column" }}>
@@ -35,35 +21,63 @@ export default function Player() {
           // controls
           ref={videoElement}
           onTimeUpdate={handleLiveProgress}
-
           src="https://s3.eu-west-1.amazonaws.com/reviewchallenge.proctorexam.com/webm/sample.webm"
         />
-        <Box sx={{ display: "flex", flexDirection: "column", justifyContent:"space-between"}}>
-          <LinearProgress style={{height:15,}} variant="determinate" value={playerState.progress} />
-          <Box sx={{ display: "flex", alignItems: "center", justifyContent:"center" , pl: 1, pb: 1 }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <LinearProgress
+            style={{ height: 15 }}
+            variant="determinate"
+            value={playerState.progress}
+          />
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              pl: 1,
+              pb: 1,
+            }}
+          >
+            
             <Box>
-            <IconButton 
-            disabled={playerState.speed === .5 } 
-            onClick={()=>{handleSpeed("mins")}} aria-label="slow down">
-              <FastRewindIcon />
-            </IconButton>
-            <IconButton onClick={togglePlay} aria-label="play/pause">
-              {!playerState.isPlaying ? 
-              <PlayArrowIcon sx={{ height: 38, width: 38 }} />
-              :<PauseIcon sx={{ height: 38, width: 38 }} />
-            }
-            </IconButton>
-            <IconButton 
-            disabled={playerState.speed === 4 } 
-              onClick={()=>{handleSpeed("plus")}}
-             aria-label="speed up">
-              <FastForwardIcon />
-            </IconButton>
+              <IconButton
+                disabled={playerState.speed === 0.5}
+                onClick={() => {
+                  handleSpeed("mins");
+                }}
+                aria-label="slow down"
+              >
+                <FastRewindIcon />
+              </IconButton>
+              <IconButton onClick={togglePlay} aria-label="play/pause">
+                {!playerState.isPlaying ? (
+                  <PlayArrowIcon sx={{ height: 38, width: 38 }} />
+                ) : (
+                  <PauseIcon sx={{ height: 38, width: 38 }} />
+                )}
+              </IconButton>
+              <IconButton
+                disabled={playerState.speed === 4}
+                onClick={() => {
+                  handleSpeed("plus");
+                }}
+                aria-label="speed up"
+              >
+                <FastForwardIcon />
+              </IconButton>
             </Box>
-            <Box> <IconButton><SpeedIcon/> {playerState.speed} x</IconButton></Box>
+            <Box>
+              <IconButton>
+                <SpeedIcon /> {playerState.speed} x
+              </IconButton>
+            </Box>
           </Box>
-
-          
         </Box>
       </Card>
     </Container>
